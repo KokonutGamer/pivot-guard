0fe3e0843443a2aaa739d4f0f0b77b3f93c158b5
Gabe Lapingcao <mgultra2018@gmail.com>
2025-04-04 19:16:47 -0700

    Background updated

--- background.js
+++ background.js
@@ -4,3 +4,22 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         console.log("New URL: ", changeInfo.url);
     }
 });
+
+chrome.webNavigation.onCompleted.addListener(function (details) {
+    // List of URLs or domains to block
+    const blockedUrls = [
+        "google.com",
+        "tracking-website.com",
+        "another-tracked-site.com"
+    ];
+
+    const currentUrl = details.url;
+
+    // Check if the current URL is in the blocked list
+    for (let url of blockedUrls) {
+        if (currentUrl.includes(url)) {
+            chrome.tabs.update(details.tabId, { url: chrome.runtime.getURL("redirect/index.html") });
+            break;
+        }
+    }
+}, { url: [{ schemes: ['http', 'https'] }] });
