8801816f00dc36b3afba9b630ed3e91273ec6390
Gabe Lapingcao <mgultra2018@gmail.com>
2025-04-04 18:56:16 -0700

    Updated styles

--- background.js
+++ background.js
@@ -1,11 +1,6 @@
-import { checkDomain } from "./src/client";
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    console.log("Extension working in background");
+    console.log("Extension working in the background");
     if (changeInfo.url) {
-        console.log("New URL:", changeInfo.url);
-        
-        checkDomain(changeInfo.url).then(result => console.log('Result: ', result)).catch(error => console.error('Error!'));
+        console.log("New URL: ", changeInfo.url);
     }
 });
-
