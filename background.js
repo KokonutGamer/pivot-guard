e810319bcad5d0db99ca59b64c6aaa3ca3697f07
Gabe Lapingcao <mgultra2018@gmail.com>
2025-04-04 18:34:19 -0700

    Huge revamp (too much stress grrr)

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
+import { checkDomain } from "./src/client";
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     console.log("Extension working in background");
     if (changeInfo.url) {
         console.log("New URL:", changeInfo.url);
-        // You can add your logic here to handle specific URLs
-        if (changeInfo.url.includes("example.com")) {
-            console.log("You're visiting example.com!");
-        }
+        
+        checkDomain(changeInfo.url).then(result => console.log('Result: ', result)).catch(error => console.error('Error!'));
     }
 });
 
---
+++ client.js
@@ -0,0 +1,14 @@
+const client = {
+    // joke API just for testing
+    async fetchRandomJoke() {
+        try {
+            const response = await fetch('https://official-joke-api.appspot.com/random_joke');
+            if (!response.ok) throw new Error("Network response was not ok");
+            const data = await response.json();
+            return `${data.setup} ${data.punchline}`;
+        } catch (error) {
+            console.error("Failed to fetch joke:", error);
+            return "Oops! Couldn't fetch a joke right now.";
+        }
+    }
+};
